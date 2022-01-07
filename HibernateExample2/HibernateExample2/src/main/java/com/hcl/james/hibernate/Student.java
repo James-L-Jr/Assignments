e471b0f240229fd6533b2dd8d427b78a0ba04ebf
James-L-Jr <james.f.logan2@gmail.com>
2022-01-07 07:23:39 -0600

    Hibernate SQL connection

---
+++ HibernateExample2/HibernateExample2/src/main/java/com/hcl/james/hibernate/Student.java
@@ -0,0 +1,53 @@
+package com.hcl.james.hibernate;
+
+public class Student {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String gradeLevel;
+
+	public Student() {
+	}
+
+	public Student(int id, String firstName, String lastName, String gradeLevel) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gradeLevel = gradeLevel;
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getGradeLevel() {
+		return gradeLevel;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setGradeLevel(String gradeLevel) {
+		this.gradeLevel = gradeLevel;
+	}
+
+}
